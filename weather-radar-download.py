bd9f032773cd76be1021cb6ab7c6df49a807d4cc
Raffaele Montella <raffaele.montella@uniparthenope.it>
2026-01-18 18:18:44 +0100

    Refactor retry logic and update product list

--- weather-radar-download.py
+++ weather-radar-download.py
@@ -12,6 +12,7 @@ import logging
 import magic
 from websockets.sync.client import connect
 
+max_trys = 7
 websocket_url = "ws://localhost:8765/update"
 url = "https://radar-api.protezionecivile.it/wide/product/downloadProduct"
 base_url = "https://data.meteo.uniparthenope.it/instruments/rdr0/"
@@ -23,20 +24,16 @@ logging.basicConfig(filename='weather-radar-download.log', encoding='utf-8', lev
 
 logger.info('Italian Civil Protection Weather Radar Smart Downloader')
 
-#products = [ "VMI", "SRI", "SRT1" ]
+# Products available "VMI", "SRI", "SRT1"
 products = [ "VMI" ]
 
 local = pytz.timezone("Europe/Rome")
 
 # storing the current time in the variable
 current_time = datetime.now()
-#current_time = datetime(2025, 3, 18, 22, 10, 00)
 
 # Get the minutes
 minutes =  float(current_time.strftime('%M'))
-
-
-#print(base_path, current_time, int(minutes/10), minutes/10)
 
 # Check if the minutes are 00, 10, ..., 50
 if int(minutes/10) == minutes/10:
@@ -54,13 +51,10 @@ if int(minutes/10) == minutes/10:
 
     file_path = base_path + "/" + utc_current_time.strftime('%Y') + "/" + utc_current_time.strftime('%m') + "/" + utc_current_time.strftime('%d')
 
-    #print(file_path)
     if not os.path.exists(file_path):
         os.makedirs(file_path)
 
     file_name = "rdr0_d01_" + utc_current_time.strftime('%Y%m%dZ%H%M') + ".tiff"
-
-    #file_path ="/home/ccmmma/prometeo/dev/weather-radar"
 
     headers = {
         "accept": "application/json, text/plain, */*",
@@ -88,7 +82,7 @@ if int(minutes/10) == minutes/10:
 
         trys = 0
 
-        while trys < 7:
+        while trys < max_trys:
 
             logger.info("Try number:" + str(trys))
 
